149e5a180eef865394f20adb5a9d115bbc9bafba
agesquivel <agesquivel@gmail.com>
2018-06-08 13:51:46 -0500

    Manejo básico de JCDB con Java

--- ConexionBdMySQL/src/ConexionBD.java
+++ ConexionBdMySQL/src/ConexionBD.java
@@ -1,15 +1,20 @@
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-
 public class ConexionBD {
         public String db = "bdBoletaje";
-        public String url = "jdbc:mysql://localhost/" + db;
+        public String url = "jdbc:mysql://localhost/" + db
+                + "?useUnicode=true"
+                + "&useJDBCCompliantTimezoneShift=true"
+                + "&useLegacyDatetimeCode=false"
+                + "&serverTimezone=UTC";
         public String user = "root";
         public String pass = "clvprinc";
 
+        Connection link;
+
         public Connection Conectar(){
-            Connection link = null;
+            link = null;
 
             try{
                 //Class.forName("org.gjt.mm.mysql.Driver");  //version 5
@@ -22,5 +27,66 @@ public class ConexionBD {
 
             return link;
         }
+
+        public int insertarCliente(String nombre, String apellidos, String direccion){
+            int numRegs = 0;
+            try {
+                PreparedStatement stInsertar = link.prepareStatement(
+                        "insert into cliente(nombre, apellidos, direccion)"
+                                + " values(?, ?, ?)");
+
+                stInsertar.setString(1, nombre);
+                stInsertar.setString(2, apellidos);
+                stInsertar.setString(3, direccion);
+
+                numRegs = stInsertar.executeUpdate();
+            }
+            catch(SQLException error){
+                error.printStackTrace();
+            }
+
+            return numRegs;
+        }
+
+    public ResultSet consultarClientes(){
+        ResultSet registros = null;
+        try {
+            PreparedStatement stConsultar = link.prepareStatement(
+                    "Select nombre, apellidos, direccion from cliente order by clienteID");
+
+            registros = stConsultar.executeQuery();
+        }
+        catch(SQLException error){
+            error.printStackTrace();
+        }
+
+        return registros;
+    }
+
+    public ResultSet consultarClienteAp(String parAp){
+        ResultSet registros = null;
+        try {
+            PreparedStatement stConsultaAp = link.prepareStatement(
+                    "Select nombre, apellidos, direccion " +
+                            "from cliente " +
+                            "where apellidos like ? "
+                            +"order by clienteID");
+            stConsultaAp.setString(1, "%" + parAp+ "%");
+            registros = stConsultaAp.executeQuery();
+        }
+        catch(SQLException error){
+            error.printStackTrace();
+        }
+
+        return registros;
+    }
 }
+
+
+
+
+
+
+
+
 
--- src/Principal.java
+++ src/Principal.java
@@ -3,10 +3,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Principal extends JFrame implements ActionListener{
 
     JButton btnConectar;
+    JButton btnInsertar;
+    JButton btnConsultar;
+    JButton btnConsultarAp;
+
+    ConexionBD interfazMysql;
 
     public Principal(){
         //Creación de interfaz
@@ -21,25 +28,96 @@ public class Principal extends JFrame implements ActionListener{
         //Construir la interfaz
         //Crear botón para conectar
         btnConectar = new JButton("Conectar");
+        btnConectar.setActionCommand("acConectar");
         btnConectar.addActionListener(this);
         contPrinc.add(btnConectar, BorderLayout.NORTH);
 
+        //Crear botón para insertar
+        btnInsertar = new JButton("Insertar");
+        btnInsertar.setActionCommand("acInsertar");
+        btnInsertar.addActionListener(this);
+        contPrinc.add(btnInsertar, BorderLayout.WEST);
+
+        //Crear botón para Consultar
+        btnConsultar = new JButton("Consultar");
+        btnConsultar.setActionCommand("acConsultar");
+        btnConsultar.addActionListener(this);
+        contPrinc.add(btnConsultar, BorderLayout.EAST);
+
+        //Crear botón para ConsultaPorAp
+        btnConsultarAp = new JButton("Consultar por apellido");
+        btnConsultarAp.setActionCommand("acConsultarAp");
+        btnConsultarAp.addActionListener(this);
+        contPrinc.add(btnConsultarAp, BorderLayout.SOUTH);
+
         ventana.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("acConectar")) {
+            //Usar la conexión BD
+            interfazMysql = new ConexionBD();
+            Connection bdCon = interfazMysql.Conectar();
+
+            if (bdCon != null) {
+                JOptionPane.showMessageDialog(null,
+                        "Conectado");
+            }
+        }
+        else if (e.getActionCommand().equals("acInsertar")) {
+            int nRegs = interfazMysql.insertarCliente("Sonia", "Acevedo Ortíz",
+                    "Av. Héroes #100 Col. Centro, Chetumal" );
+            System.out.println("Núm de registro afectados: " + nRegs);
+        }
+        else if (e.getActionCommand().equals("acConsultar")) {
+            ResultSet regs = interfazMysql.consultarClientes();
+            int cont = 0;
+            try {
+                while (regs.next()) {
+                    cont++;
+                    System.out.println("Núm " + cont + ": "
+                            + regs.getString("nombre") + ", "
+                            + regs.getString("apellidos") + ", "
+                            + regs.getString("direccion"));
+                }
+            }
+            catch (SQLException errSelect){
+                errSelect.printStackTrace();
+            }
+        }
+        else if (e.getActionCommand().equals("acConsultarAp")) {
+            ResultSet regs = interfazMysql.consultarClienteAp("ez");
+            int cont = 0;
+
+            System.out.println("Registros recuperados");
+            try {
+                while (regs.next()) {
+                    cont++;
+                    System.out.println("Núm " + cont + ": "
+                            + regs.getString("nombre") + ", "
+                            + regs.getString("apellidos") + ", "
+                            + regs.getString("direccion"));
+                }
+            }
+            catch (SQLException errSelect){
+                errSelect.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args){
         Principal vtnPrinc = new Principal();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        //Usar la conexión BD
-        ConexionBD con = new ConexionBD();
-        Connection bdCon = con.Conectar();
-
-        if (bdCon != null){
-            JOptionPane.showMessageDialog(null,
-                    "Conectado");
-        }
-    }
 }
+
+
+
+
+
+
+
+
+
+
